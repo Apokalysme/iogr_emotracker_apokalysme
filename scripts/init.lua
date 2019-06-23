442c48b4c9d8f4e94942e0a4facb2e54457a5260
Apokalysme <apokalysme@gmail.com>
2019-06-23 15:10:05 +0200

    Code optimization and minor fixes

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,13 +1,15 @@
 --  Load configuration options up front
 ScriptHost:LoadScript("scripts/settings.lua")
 
-Tracker:AddItems("items/options.json")
-Tracker:AddItems("items/common.json")
-Tracker:AddItems("items/dungeon_items.json")
+Tracker:AddItems("items/items.json")
+Tracker:AddItems("items/abilities.json")
+Tracker:AddItems("items/objectives.json")
 Tracker:AddItems("items/dark_spaces.json")
 Tracker:AddItems("items/labels.json")
 
 Tracker:AddMaps("maps/maps.json")
+
+Tracker:AddLocations("locations/logic.json")
 Tracker:AddLocations("locations/world.json")
 Tracker:AddLocations("locations/dungeons.json")
 
