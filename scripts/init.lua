f79396cb7b1f9e56534793439e9f91729f826927
Apokalysme <apokalysme@gmail.com>
2019-06-24 21:45:07 +0200

    Upload v1.3.0.0 in progress
    v1.3.0.0 (in progress)
    * General :
      * Code reorganization and optimization
    * Item Tracker :
      * Change : Separation between "Kara saved" icon and "Kara's Paint" location icon
      * Fix : Mine keys, A and B, were switched
      * Fix : Hieroglyphs 2 and 3 were switched
    * Map Tracker :
      * Add : Kara's Paint will appear on the Map depending on Kara's Paint location icon (appearance for minor dungeons)
      * Add : Kara's Paint will be considered accessible once magic dust is obtained (without it, it won't appear)
      * Add : Including different world maps (Full and by continent)
      * Add WIP : For each continent, including areas maps
      * Fix : Logic to access Top of Babel

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,18 +1,49 @@
 --  Load configuration options up front
 ScriptHost:LoadScript("scripts/settings.lua")
 
+-- Progression items
 Tracker:AddItems("items/items.json")
+-- Characters' abilities
 Tracker:AddItems("items/abilities.json")
+-- Adventure's Objectives
 Tracker:AddItems("items/objectives.json")
+-- Dark Spaces
 Tracker:AddItems("items/dark_spaces.json")
+-- Labels (not used for now)
 Tracker:AddItems("items/labels.json")
 
+-- World maps
 Tracker:AddMaps("maps/maps.json")
+-- Areas maps
+Tracker:AddMaps("maps/areas.json")
 
+-- Global logic
 Tracker:AddLocations("locations/logic.json")
-Tracker:AddLocations("locations/world.json")
-Tracker:AddLocations("locations/dungeons.json")
+-- Jeweler's locations
+Tracker:AddLocations("locations/jeweler.json")
+-- South West Continent
+Tracker:AddLocations("locations/south_west/cities.json")
+Tracker:AddLocations("locations/south_west/dungeons.json")
+-- South East Continent
+Tracker:AddLocations("locations/south_east/cities.json")
+Tracker:AddLocations("locations/south_east/dungeons.json")
+-- North East Continent
+Tracker:AddLocations("locations/north_east/seaside_palace.json")
+Tracker:AddLocations("locations/north_east/cities.json")
+Tracker:AddLocations("locations/north_east/dungeons.json")
+-- North Continent
+Tracker:AddLocations("locations/north/cities.json")
+Tracker:AddLocations("locations/north/dungeons.json")
+-- North West Continent
+Tracker:AddLocations("locations/north_west/cities.json")
+Tracker:AddLocations("locations/north_west/dungeons.json")
+-- Babel
+Tracker:AddLocations("locations/babel/dungeons.json")
 
+-- Dummy settings (for the future)
+-- Tracker:AddItems("layouts/settings.json")
+-- Settings grid
 Tracker:AddLayouts("layouts/options_grid.json")
+-- Layout
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/standard_broadcast.json")
