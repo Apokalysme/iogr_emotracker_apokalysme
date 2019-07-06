32ec97c4d58fd16f981472a8cbd246d1c279094b
Apokalysme <apokalysme@gmail.com>
2019-07-06 18:28:13 +0200

    Item Table and some note in Changelog

--- autotracking/autotracking.lua
+++ autotracking/autotracking.lua
@@ -8,7 +8,6 @@ print("")
 print("Active Auto-Tracker Configuration")
 print("---------------------------------------------------------------------")
 print("Enable Item Tracking:        ", AUTOTRACKER_ENABLE_ITEM_TRACKING)
-print("Enable Location Tracking:    ", AUTOTRACKER_ENABLE_LOCATION_TRACKING)
 if AUTOTRACKER_ENABLE_DEBUG_LOGGING then
     print("Enable Debug Logging:        ", "true")
 end
@@ -20,10 +19,6 @@ function autotracker_started()
 end
 
 -- Initial values
--- Are we on the Moon ?
-AUTOTRACKER_IS_ON_THE_MOON = false
--- Have we done post game summary ?
-AUTOTRACKER_HAS_DONE_POST_GAME_SUMMARY = false
 
 -- Clean memory read values
 U8_READ_CACHE = 0
@@ -31,6 +26,55 @@ U8_READ_CACHE_ADDRESS = 0
 
 U16_READ_CACHE = 0
 U16_READ_CACHE_ADDRESS = 0
+
+
+ITEM_TABLE = {
+	1 = "red_jewel"
+	2 = "prison_key"
+	3 = "inca_statue_a"
+	4 = "inca_statue_b"
+	6 = "herb"
+	7 = "diamond_block"
+	8 = "melody_wind"
+	9 = "melody_lola"
+	10 = "large_roast"
+	11 = "mine_key_a"
+	12 = "mine_key_b"
+	13 = "melody_memory"
+	14 = "cristal_ball"
+	15 = "elevator_key"
+	16 = "mu_key"
+	17 = "purity_stone"
+	18 = "hope_statue"
+	19 = "rama_statue"
+	20 = "magic_dust"
+	22 = "letter_lance"
+	23 = "necklace"
+	24 = "will"
+	25 = "teapot"
+	26 = "mushroom_drop"
+	28 = "black_glasses"
+	29 = "gorgon_flower"
+	30 = "hieroglyph_5"
+	31 = "hieroglyph_4"
+	32 = "hieroglyph_3"
+	33 = "hieroglyph_1"
+	34 = "hieroglyph_6"
+	35 = "hieroglyph_2"
+	36 = "aura"
+	37 = "letter_lola"
+	38 = "journal"
+	39 = "crystal_ring"
+	40 = "apple"
+	41 = "hp_jewel"
+	42 = "defense_jewel"
+	43 = "strength_jewel"
+	44 = "light_jewel"
+	45 = "dark_jewel"
+	46 = "2_red_jewels"
+	47 = "3_red_jewels"
+	56 = "???"
+}
 
 
 -- Reset cache addresses
@@ -68,35 +112,18 @@ function isInGame()
     return AutoTracker:ReadU8(0x7e0010, 0) > 0x05
 end
 
--- Update InGame Status by reading memory segment
-function updateInGameStatusFromMemorySegment(segment)
-
-	-- set up "0x7e0010" memory segment
-    local mainModuleIdx = segment:ReadUInt8(0x7e0010)
-
-	-- save last state
-    local wasOnTheMoon = AUTOTRACKER_IS_ON_THE_MOON
-	-- check if player is on the moon
-    AUTOTRACKER_IS_IN_TRIFORCE_ROOM = (mainModuleIdx == 0x19 or mainModuleIdx == 0x1a)
-
-	-- if player is now in Triforce Room and was not before
-    if AUTOTRACKER_IS_ON_THE_MOON and not wasOnTheMoon then
-		-- take statistics in "0x7ef420" segment at "0x46" address
-        ScriptHost:AddMemoryWatch("IoG Statistics", 0x7ef420, 0x46, updateStatisticsFromMemorySegment)
-    end
-
-	-- if Debug mode enabled
-    if AUTOTRACKER_ENABLE_DEBUG_LOGGING then
-		-- track current room information
-        if mainModuleIdx > 0x05 then
-            print("Current Room Index: ", segment:ReadUInt16(0x7e00a0))
-            print("Current OW   Index: ", segment:ReadUInt16(0x7e008a))
-        end
-        return false
-    end
-
-    return true
+function lastItemAddedInInventory()
+	local value = ReadU8(segment, 0x7e0db8)
+	
+	item = whatItemIsIt(value)
+	
+	return item
 end
+
+function inventoryCount()
+	
+end
+
 
 -- update "progressive" item depending on memory state
 function updateProgressiveItemFromByte(segment, code, address, offset)
@@ -303,40 +330,6 @@ function updateSectionChestCountFromByteAndFlag(segment, locationRef, address, f
     end
 end
 
-function updateSectionChestCountFromOverworldIndexAndFlag(segment, locationRef, index, callback)
-    updateSectionChestCountFromByteAndFlag(segment, locationRef, 0x7ef280 + index, 0x40, callback)
-end
-
--- update chest state from memory
-function updateSectionChestCountFromRoomSlotList(segment, locationRef, roomSlots, callback)
-    local location = Tracker:FindObjectForCode(locationRef)
-    if location then
-        -- Do not auto-track this the user has manually modified it
-        if location.Owner.ModifiedByUser then
-            return
-        end
-
-        local clearedCount = 0
-        for i,slot in ipairs(roomSlots) do
-            local roomData = ReadU16(segment, 0x7ef000 + (slot[1] * 2))
-
-            if AUTOTRACKER_ENABLE_DEBUG_LOGGING then
-                print(locationRef, roomData, 1 << slot[2])
-            end
-                
-            if (roomData & (1 << slot[2])) ~= 0 then
-                clearedCount = clearedCount + 1
-            end
-        end
-
-        location.AvailableChestCount = location.ChestCount - clearedCount
-
-        if callback then
-            callback(clearedCount > 0)
-        end
-    end
-end
-
 function updateNPCItemFlagsFromMemorySegment(segment)
 
     if not isInGame() then
@@ -366,94 +359,6 @@ function updateNPCItemFlagsFromMemorySegment(segment)
     updateSectionChestCountFromByteAndFlag(segment, "@Witch's Hut/Assistant", 0x7ef411, 0x20, updateMushroomStatus)
     -- 0x40 is unused
     updateSectionChestCountFromByteAndFlag(segment, "@Magic Bat/Magic Bowl", 0x7ef411, 0x80, updateBatIndicatorStatus)    
-
-end
-
-function updateOverworldEventsFromMemorySegment(segment)
-    
-    if not isInGame() then
-        return false
-    end
-
-    if not AUTOTRACKER_ENABLE_LOCATION_TRACKING then
-        return true
-    end    
-
-    InvalidateReadCaches()
-
-    updateSectionChestCountFromOverworldIndexAndFlag(segment, "@Spectacle Rock/Up On Top",              3)    
-    updateSectionChestCountFromOverworldIndexAndFlag(segment, "@Floating Island/Island",                5)    
-    updateSectionChestCountFromOverworldIndexAndFlag(segment, "@Race Game/Take This Trash",             40)    
-    updateSectionChestCountFromOverworldIndexAndFlag(segment, "@Grove Digging Spot/Hidden Treasure",    42)    
-    updateSectionChestCountFromOverworldIndexAndFlag(segment, "@Desert Ledge/Ledge",                    48)    
-    updateSectionChestCountFromOverworldIndexAndFlag(segment, "@Lake Hylia Island/Island",              53)    
-    updateSectionChestCountFromOverworldIndexAndFlag(segment, "@Dam/Outside",                           59)    
-    updateSectionChestCountFromOverworldIndexAndFlag(segment, "@Bumper Cave/Ledge",                     74)    
-    updateSectionChestCountFromOverworldIndexAndFlag(segment, "@Pyramid Ledge/Ledge",                   91)    
-    updateSectionChestCountFromOverworldIndexAndFlag(segment, "@Digging Game/Dig For Treasure",         104)    
-    updateSectionChestCountFromOverworldIndexAndFlag(segment, "@Master Sword Pedestal/Pedestal",        128)    
-    updateSectionChestCountFromOverworldIndexAndFlag(segment, "@Zora Area/Ledge",                       129)    
-end
-
-function updateRoomsFromMemorySegment(segment)
-
-    if not isInGame() then
-        return false
-    end
-
-    InvalidateReadCaches()
-
-    if AUTOTRACKER_ENABLE_ITEM_TRACKING then
-        updateToggleFromRoomSlot(segment, "mystic_statue_1", { 2591, 11 })
-        updateToggleFromRoomSlot(segment, "mystic_statue_2", { 2591, 11 })
-        updateToggleFromRoomSlot(segment, "mystic_statue_3", { 2591, 11 })
-        updateToggleFromRoomSlot(segment, "mystic_statue_4", { 2591, 11 })
-        updateToggleFromRoomSlot(segment, "mystic_statue_5", { 2591, 11 })
-        updateToggleFromRoomSlot(segment, "mystic_statue_6", { 2591, 11 })
-    end
-
-    if not AUTOTRACKER_ENABLE_LOCATION_TRACKING then
-        return true
-    end
-
-    updateSectionChestCountFromRoomSlotList(segment, "@Link's House/By The Door", { { 0, 10 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@The Well/Cave", { { 47, 5 }, { 47, 6 }, { 47, 7 }, { 47, 8 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@The Well/Bombable Wall", { { 47, 4 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Hookshot Cave/Bonkable Chest", { { 60, 7 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Hookshot Cave/Back", { { 60, 4 }, { 60, 5 }, { 60, 6 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Castle Secret Entrance/Hallway", { { 85, 4 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Lost Woods/Forest Hideout", { { 225, 9 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Lumberjack Cave/Cave", { { 226, 9 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Spectacle Rock/Cave", { { 234, 10 } })        
-    updateSectionChestCountFromRoomSlotList(segment, "@Paradox Cave/Top", { { 239, 4 }, { 239, 5 }, { 239, 6 }, { 239, 7 }, { 239, 8 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Super-Bunny Cave/Cave", { { 248, 4 }, { 248, 5 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Spiral Cave/Cave", { { 254, 4 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Paradox Cave/Bottom", { { 255, 4 }, { 255, 5 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Tavern/Back Room", { { 259, 4 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Link's House/By The Door", { { 260, 4 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Sahasrala's Hut/Back Room", { { 261, 4 }, { 261, 5 }, { 261, 6 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Bombable Shack/Downstairs", { { 262, 4 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Treasure Game/Prize", { { 262, 10 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Chicken House/Bombable Wall", { { 264, 4 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Aginah's Cave/Cave", { { 266, 4 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Dam/Inside", { { 267, 4 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Mimic Cave/Cave", { { 268, 4 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Mire Shack/Shack", { { 269, 4 }, { 269, 5 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@King's Tomb/The Crypt", { { 275, 4 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Waterfall Fairy/Waterfall Cave", { { 276, 4 }, { 276, 5 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Fat Fairy/Big Bomb Spot", { { 278, 4 }, { 278, 5 } }, updateBombIndicatorStatus)
-    updateSectionChestCountFromRoomSlotList(segment, "@Spike Cave/Cave", { { 279, 4 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Graveyard Ledge/Cave", { { 283, 9 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@South of Grove/Circle of Bushes", { { 283, 10 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@C-Shaped House/House", { { 284, 4 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Blind's House/Basement", { { 285, 5 }, { 285, 6 }, { 285, 7 }, { 285, 8 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Blind's House/Bombable Wall", { { 285, 4 } })   
-    updateSectionChestCountFromRoomSlotList(segment, "@Hype Cave/Cave", { { 286, 4 }, { 286, 5 }, { 286, 6 }, { 286, 7 }, { 286, 10 } }) 
-    updateSectionChestCountFromRoomSlotList(segment, "@Ice Rod Cave/Cave", { { 288, 4 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Mini Moldorm Cave/Cave", { { 291, 4 }, { 291, 5 }, { 291, 6 }, { 291, 7 }, { 291, 10 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Bonk Rocks/Cave", { { 292, 4 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Checkerboard Cave/Cave", { { 294, 9 } })
-    updateSectionChestCountFromRoomSlotList(segment, "@Hammer Pegs/Cave", { { 295, 10 } })
 
 end
 
@@ -525,102 +430,5 @@ function updateItemsFromMemorySegment(segment)
 
 end
 
-function updateHeartPiecesFromMemorySegment(segment)
-
-    if not isInGame() then
-        return false
-    end
-
-    InvalidateReadCaches()
-
-    if AUTOTRACKER_ENABLE_ITEM_TRACKING then
-        updateConsumableItemFromByte(segment, "heartpiece", 0x7ef448)
-    end
-end
-
-function updateHeartContainersFromMemorySegment(segment)
-
-    if not isInGame() then
-        return false
-    end
-
-    InvalidateReadCaches()
-
-    if AUTOTRACKER_ENABLE_ITEM_TRACKING then
-
-        local pieces = Tracker:FindObjectForCode("heartpiece")
-        local containers = Tracker:FindObjectForCode("heartcontainer")
-
-        if pieces and containers then
-            
-            local maxHealth = (ReadU8(segment, 0x7ef36c) // 8) - 3
-            
-            if AUTOTRACKER_ENABLE_DEBUG_LOGGING then
-                print("Pieces: ", pieces.AcquiredCount)
-                print("Max Health: ", maxHealth)
-            end
-
-            containers.AcquiredCount = maxHealth - (pieces.AcquiredCount // 4)
-        end
-    end
-end
-
-AUTOTRACKER_STATS_MARKDOWN_FORMAT = [===[
-### Post-Game Summary
-
-Stat | Value
---|-
-**Collection Rate** | %d/216
-**Deaths** | %d
-**Bonks** | %d
-**Total Time** | %02d:%02d:%02d.%02d
-]===]
-
-function read32BitTimer(segment, baseAddress)
-    local timer = 0;
-    timer = timer | (ReadU8(segment, baseAddress + 3) << 24)
-    timer = timer | (ReadU8(segment, baseAddress + 2) << 16)
-    timer = timer | (ReadU8(segment, baseAddress + 1) << 8)
-    timer = timer | (ReadU8(segment, baseAddress + 0) << 0)
-
-    local hours = timer // (60 * 60 * 60)
-    local minutes = (timer % (60 * 60 * 60)) // (60 * 60)
-    local seconds = (timer % (60 * 60)) // (60)
-    local frames = timer % 60
-
-    return hours, minutes, seconds, frames
-end
-
-function updateStatisticsFromMemorySegment(segment)
-
-    if not isInGame() then
-        return false
-    end
-
-    InvalidateReadCaches()
-
-    if not AUTOTRACKER_HAS_DONE_POST_GAME_SUMMARY then
-        -- Read completion timer
-        local hours, minutes, seconds, frames = read32BitTimer(segment, 0x7ef43e)
-
-        local collection_rate = ReadU8(segment, 0x7ef423)
-        local deaths = ReadU8(segment, 0x7ef449)
-        local bonks = ReadU8(segment, 0x7ef420)
-
-        local markdown = string.format(AUTOTRACKER_STATS_MARKDOWN_FORMAT, collection_rate, deaths, bonks, hours, minutes, seconds, frames)
-        ScriptHost:PushMarkdownNotification(NotificationType.Celebration, markdown)
-    end
-
-    AUTOTRACKER_HAS_DONE_POST_GAME_SUMMARY = true
-
-    return true
-end
-
--- Run the in-game status check more frequently (every 250ms) to catch save/quit scenarios more effectively
-ScriptHost:AddMemoryWatch("LTTP In-Game status", 0x7e0010, 0x90, updateInGameStatusFromMemorySegment, 250)
-ScriptHost:AddMemoryWatch("LTTP Item Data", 0x7ef340, 0x90, updateItemsFromMemorySegment)
-ScriptHost:AddMemoryWatch("LTTP Room Data", 0x7ef000, 0x250, updateRoomsFromMemorySegment)
-ScriptHost:AddMemoryWatch("LTTP Overworld Event Data", 0x7ef280, 0x82, updateOverworldEventsFromMemorySegment)
-ScriptHost:AddMemoryWatch("LTTP NPC Item Data", 0x7ef410, 2, updateNPCItemFlagsFromMemorySegment)
-ScriptHost:AddMemoryWatch("LTTP Heart Piece Data", 0x7ef448, 1, updateHeartPiecesFromMemorySegment)
-ScriptHost:AddMemoryWatch("LTTP Heart Container Data", 0x7ef36c, 1, updateHeartContainersFromMemorySegment)
+ScriptHost:AddMemoryWatch("IoG Item Data", 0x7ef340, 0x90, updateItemsFromMemorySegment)
+ScriptHost:AddMemoryWatch("IoG NPC Item Data", 0x7ef410, 2, updateNPCItemFlagsFromMemorySegment)
